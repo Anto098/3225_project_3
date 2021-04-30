b75ec85c5b447ed899f20173165a10c121db94d8
Anto <antonin.roy1@gmail.com>
2021-04-30 19:39:20 -0400

    working state php request

--- config-freq.php
+++ config-freq.php
@@ -2,7 +2,14 @@
 
 include("pwd.php");
 
+$db_user = "anto";
+$db_host = "localhost";
+$db_name = "ift3225";
+$db_table = "usf_fan";
+/*
 $db_user = "royanton";
-$db_host = "www-ens.iro.umontreal.ca";
+$db_host = "www-ens";
 $db_name = "royanton_ift3225";
 $db_table = "usf_fan";
+*/
+
