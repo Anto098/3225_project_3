7561fcd97c437b3bc7ca9f39d167d53c1232d153
Jinkoe <hugo@baudchon.com>
2021-05-01 15:16:28 -0300

    setup mysql

--- config-freq.php
+++ config-freq.php
@@ -2,7 +2,17 @@
 
 include("pwd.php");
 
+<<<<<<< Updated upstream
 $db_user = "baudchoh";
 $db_host = "mysql.iro.umontreal.ca";
+=======
+$db_user = "root";
+$db_host = "localhost";
+$db_name = "projet3";
+$db_table = "usf_fan";
+/*
+$db_user = "royanton";
+$db_host = "www-ens";
+>>>>>>> Stashed changes
 $db_name = "royanton_ift3225";
 $db_table = "usf_fan";
