11bc2899699cb10be1aba02aa6e1ddd0b6dfc1bf
Jinkoe <hugo@baudchon.com>
2021-05-01 15:20:03 -0300

    setup mysql

--- config-freq.php
+++ config-freq.php
@@ -2,7 +2,7 @@
 
 include("pwd.php");
 
-$db_user = "baudchoh";
-$db_host = "mysql.iro.umontreal.ca";
-$db_name = "royanton_ift3225";
+$db_user = "root";
+$db_host = "localhost";
+$db_name = "projet3";
 $db_table = "usf_fan";
